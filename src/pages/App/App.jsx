28e6acac3994e6342a488d81c10b027f0d102af6
crice802 <rcrice@gmail.com>
2021-08-14 14:03:39 -0400

    conflict resolved

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,57 +1,121 @@
-import React, { Component } from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+} from "@react-google-maps/api";
+import mapStyles from "./mapStyles";
 
-import NavBar from '../../components/NavBar/NavBar'
-import Signup from '../Signup/Signup'
-import Login from '../Login/Login'
-import Landing from '../Landing/Landing'
-import Users from '../Users/Users'
-import StateLocation from '../../components/StateLocaltion/StateLocation'
-import * as authService from '../../services/authService'
+const libraries = ["places"];
+const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw",
+};
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+};
 
 
-class App extends Component {
-	state = {
-		user: authService.getUser()
-	}
+const googleMapsApiKey = process.env.REACT_APP_API_KEY_GOOGLE_MAPS
+const tomtomApiKey = process.env.REACT_APP_API_KEY_TOMTOM
 
-	handleLogout = () => {
-		authService.logout()
-		this.setState({ user: null })
-		this.props.history.push('/')
-	}
+const Map = (props) => {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: googleMapsApiKey,
+        libraries,
+    });
+    // const [markers, setMarkers] = useState([]);
+    const [dogParks, setDogParks] = useState([])
+    const [selected, setSelected] = useState(null)
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
+    const [center, setCenter] = useState()
+    
+    useEffect(() => {
+        (async() => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setLat(parseFloat(position.coords.latitude))
+                    setLng(parseFloat(position.coords.longitude))
+                },
+                () => null
+                )
+    })()
+    }, [])
 
-	handleSignupOrLogin = () => {
-		this.setState({ user: authService.getUser() })
-	}
+    useEffect(() => {
+        const tomtom = `https://api.tomtom.com/search/2/poiSearch/%22dog%20parks%22.json?limit=100&lat=${lat}&lon=${lng}&radius=5000&key=${tomtomApiKey}`
+        const makeApiCall = async () => {
+            const res = await fetch(tomtom)
+            const {results} = await res.json();
+            setDogParks(results)
+        };
+        makeApiCall();
+    }, [lat, lng]);
 
-	render() {
-		const { user } = this.state
-		return (
-			<>
-				<NavBar user={user} handleLogout={this.handleLogout} history={this.props.history}/>
-				<Route exact path='/'>
-          <Landing user={user} 
-					render={()=> <StateLocation
-					
-					/>}
-					/>
-        </Route>
-				<Route exact path='/signup'>
-          <Signup history={this.props.history} handleSignupOrLogin={this.handleSignupOrLogin}/>
-        </Route>
-				<Route exact path='/login'>
-          <Login handleSignupOrLogin={this.handleSignupOrLogin} history={this.props.history}/>
-        </Route>
-				<Route 
-					exact path="/users"
-					render={()=> 
-						user ? <Users /> : <Redirect to='/login'/>
-				}/>
+    useEffect(() => {
+        props.location &&
 
-			</>
-		)
-	}
+        (async () => {
+            // setLat(props.location.lat)
+            // setLng(props.location.lng)
+            // console.log('LATANDLONG!!!', lat, lng)
+            const tomtom = `https://api.tomtom.com/search/2/poiSearch/%22dog%20parks%22.json?limit=100&lat=${props.location.lat}&lon=${props.location.lon}&radius=5000&key=${tomtomApiKey}`
+            const res = await fetch(tomtom)
+            const {results} = await res.json();
+            setDogParks(results)
+        })();
+        
+    
+    }, [props.location]);
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    // const panTo = React.useCallback(({ lat, lng }) => {
+    //     mapRef.current.panTo({ lat, lng });
+    //     mapRef.current.setZoom(14);
+    // }, []);
+
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
+
+
+    return (
+        <>
+            <GoogleMap
+                id="map"
+                mapContainerStyle={mapContainerStyle}
+                zoom={14}
+                initialCenter={{lat: lat, lng: lng}}
+                center={{lat: lat, lng: lng}}
+                options={options}
+                onLoad={onMapLoad}
+                // panTo={panTo}
+            >
+
+                {dogParks?.map((park) => (
+                    <Marker
+                        key={park.id}
+                        position={{ lat: park.position.lat, lng: park.position.lon }}
+                        onClick={() => {
+                            setSelected(park);
+                        }}
+                        icon={{
+                            url: `/dog-treat.png`,
+                            origin: new window.google.maps.Point(0, 0),
+                            anchor: new window.google.maps.Point(15, 15),
+                            scaledSize: new window.google.maps.Size(30, 30),
+                        }}
+                    />
+                ))}
+            </GoogleMap>
+        </>
+    )
 }
 
-export default App
+export default Map;
