df2440a3c19bf748a04d58c7850dc5cf98e135dd
crice802 <rcrice@gmail.com>
2021-08-15 14:37:16 -0400

    map inside event

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MapCard from '../MapCard/MapCard';
 
 const EventCard = (props) => {
   return ( 
@@ -7,7 +8,9 @@ const EventCard = (props) => {
     <Link>
     
     <h2>these are the events</h2>
+    
     </Link>
+    <MapCard />
     </>
   
   );
