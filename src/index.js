57d9f927ed6359fb6e1261618981636b13ada258
crice802 <rcrice@gmail.com>
2021-08-16 16:28:23 -0400

    style navbar"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css'
 import App from './pages/App/App'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <Router>
